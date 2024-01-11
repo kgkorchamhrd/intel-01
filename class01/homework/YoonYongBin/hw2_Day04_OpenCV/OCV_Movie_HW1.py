91c73625db47b5e97f324b069a6109700e055a51
Drabeen Cley <dragonturtle@naver.com>
2024-01-11 20:08:12 +0900

    This Day04 : OpenCV Homework.

---
+++ class01/homework/YoonYongBin/hw2_Day04_OpenCV/OCV_Movie_HW1.py
@@ -0,0 +1,33 @@
+import cv2
+import time
+import numpy as np
+
+print("cv2 ver:", cv2.__version__, "np ver:", np.__version__)
+
+import numpy as np
+import cv2
+
+# Read from the recorded video file
+
+cap = cv2.VideoCapture("ronaldinho.mp4")
+
+# 동영상 파일이 성공적으로 열렸으면 while 문 반복
+while(cap.isOpened()):
+# 한 프레임을 읽어옴
+
+    ret, frame = cap.read()
+
+    if ret is False:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+# Display
+    cv2.imshow("Frame",frame)
+# 1 ms 동안 대기하며 키 입력을 받고 'q' 입력 시 종료
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        break
+    
+    time.sleep(0.033)
+
+cap.release()
+cv2.destroyAllWindows()
