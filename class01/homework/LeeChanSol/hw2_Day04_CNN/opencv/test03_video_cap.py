8d1acee014d51ed588cdd006169ef5223faea204
Lee Chansol <momo790@naver.com>
2024-01-18 09:37:16 +0900

    add hw2+hw3

---
+++ class01/homework/LeeChanSol/hw2_Day04_CNN/opencv/test03_video_cap.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+
+w = 640
+h = 480
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, w)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+
+while(cap.isOpened()):
+
+    ret, frame = cap.read()
+    if ret is False:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    cv2.imshow('Camera', frame)
+
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        break
